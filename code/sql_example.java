60a646cf16b76b8be982b06b33e4cb9e8b407443
thomcz <thomas.czogalik@gmx.de>
2016-03-24 11:58:41 +0100

    some more changes

--- code/sql_example.java
+++ code/sql_example.java
@@ -3,4 +3,4 @@ stmt = connection.createStatement();@\label{sql:createStmt}@
 BufferedReader br = ...@\label{sql:createBr}@
 String name = br.readLine();@\label{sql:name}@
 String sql = "SELECT * FROM users WHERE name=" + name + ";";@\label{sql:sql}@
-stmt.executeQuery(sql);@\label{sql:execute}@
+stmt.executeQuery(sql);@\label{sql:sink}@
